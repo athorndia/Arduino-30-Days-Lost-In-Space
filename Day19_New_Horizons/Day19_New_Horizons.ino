b3a83b21ea7928d73fc2b43a2cae90d1c522fd00
Anna Thorndike <104226292+athorndia@users.noreply.github.com>
2023-01-15 13:29:52 -0800

    Add files via upload

---
+++ Day19_New_Horizons/Day19_New_Horizons.ino
@@ -0,0 +1,152 @@
+/* This example demonstrates how to connect HERO with rotary encoder and 7 segment display.
+Here the rotary encoder is an input device and the 7 segment display is an output device.
+The 7 segment display prints the values of the rotary encoder (positive or negative) on 4 digits
+where the first digit (most left) is used for the minus sign (-).
+Buzzer on breadboard. 2 wires connect buzzer(-)end to digital GND and buzzer(s) to pin 10 on HERO. 
+
+Had to switch from pc to mac and installed the following libraries: TM1637, Keypad,... */
+
+#include <TM1637Display.h>
+#include <Encoder.h>
+ 
+// Rotary Encoder Inputs connected to pins on HERO:
+#define CLK2 2
+#define DT2 3
+ 
+// Define the display connection pins for 4 digit 7 segment display connected to pins on HERO:
+#define CLK 6
+#define DIO 5
+
+// Define the "decrypted" secret numbers for audio buzzer:
+// *Original code = 23-353-1688, but I didn't want to turn it 1688x, so the numbers were modified for convenience*
+#define SECRET1 5
+#define SECRET2 10
+#define SECRET3 15 // done 
+ 
+// Create display object of type TM1637Display:
+TM1637Display OurDisplay = TM1637Display(CLK, DIO);
+ 
+// Create array that turns all segments on:
+const uint8_t data[] = {0xff, 0xff, 0xff, 0xff};
+ 
+// Create array that turns all segments off:
+const uint8_t blank[] = {0x00, 0x00, 0x00, 0x00};
+ 
+// You can set the individual segments per digit to spell words or create other symbols on 7 segment 4 digit display:
+const uint8_t done[] = {
+  SEG_B | SEG_C | SEG_D | SEG_E | SEG_G,           // d
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // O
+  SEG_C | SEG_E | SEG_G,                           // n
+  SEG_A | SEG_D | SEG_E | SEG_F | SEG_G            // E
+};
+ 
+int counter = 0;    // keep a running tally of steps...this is keeping count of increments in the rotation of the encoder:
+int currentStateCLK;
+int lastStateCLK;
+String currentDir =""; 
+int buzzer = 10;  // pin 10 of the HERO that drives the buzzer
+int toneFreq = 0; // the number of cycles per second with which we're going to activate the buzzer and will create the tone that'll unlock the "navigation system".
+
+// *************************************************************************** //
+
+// custom functions to give audio feedback
+
+void playBuzz() {
+  tone(buzzer, toneFreq, 1000);
+  delay(1000);
+  noTone(buzzer);
+  delay(100);
+  OurDisplay.clear();
+  counter++;
+}
+
+// *************************************************************************** //
+
+/* The setup routine initializes the serial monitor and sets up the interrupt function for the rotary encoder 
+to detect rotations of the encoder shaft and those interrups call an interrupt service routine whose name is "updateEncoder". */
+
+void setup() {
+ 
+  // Set encoder pins as inputs
+  pinMode(CLK2,INPUT);
+  pinMode(DT2,INPUT);
+ 
+  // Setup Serial Monitor
+  Serial.begin(9600);
+ 
+  // Read the initial state of A (CLK)
+  lastStateCLK = digitalRead(CLK2);
+ 
+  // Clear the display:
+  OurDisplay.clear();
+  delay(200);
+  OurDisplay.setBrightness(7);
+ 
+  // Call Interrupt Service Routine (ISR) updateEncoder() when any high/low change
+  // is seen on A (CLK2) interrupt  (pin 2), or B (DT2) interrupt (pin 3)
+ 
+  attachInterrupt(digitalPinToInterrupt(CLK2), updateEncoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(DT2), updateEncoder, CHANGE);
+}
+
+// *************************************************************************** //
+ 
+/* This loop routine puts information out to the serial monitor and to the 7 segment display
+ and at appropriate times send the audio signal of the correct frequency to the buzzer. */
+void loop() {
+  OurDisplay.showNumberDec(counter);
+  delay(50);
+ 
+ /* when counter reaches designated value we will set tone frequency to that value 
+ and we will play the buzzer for second. */
+  if(counter == SECRET1) {  
+    toneFreq=SECRET1;
+    playBuzz();
+  }
+ 
+ if(counter == SECRET2) {
+    toneFreq=SECRET2;
+    playBuzz();
+ } 
+
+ if(counter == SECRET3) {
+    toneFreq=SECRET3 ;
+    playBuzz();
+ }
+}
+ 
+// *************************************************************************** //
+
+/* This is our ISR (Interrupt Service Routine) which has the job of responding to interrupt events.
+It's simply used to detect and react to rotations in the shaft of the rotary encoder.  
+*/
+void updateEncoder(){
+  // Read the current state of CLK
+  currentStateCLK = digitalRead(CLK2);
+ 
+  // If last and current state of CLK are different, then a pulse occurred;
+  // React to only 0->1 state change to avoid double counting
+  if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
+ 
+    // If the DT state is different than the CLK state then
+    // the encoder is rotating CW so INCREASE counter by 1
+    if (digitalRead(DT2) != currentStateCLK) {
+      counter ++;
+      currentDir ="CW";
+     
+    } else {
+      // Encoder is rotating CCW so DECREASE counter by 1
+      counter --;
+      currentDir ="CCW";
+    }
+ 
+    Serial.print("Direction: ");
+    Serial.print(currentDir);
+    Serial.print(" | Counter= ");
+    Serial.println(counter);
+  }
+ 
+  // Remember last CLK state to use on next interrupt...
+  lastStateCLK = currentStateCLK;
+  delay(1); // Put in a slight delay to help debounce the reading. 
+}
---
+++ Day21_Hello_New_World/Day21_Hello_New_World.ino
@@ -0,0 +1,67 @@
+/* This code demonstrates how to use an OLED or Organic Light Emitting Diode Display. 
+This OLED display has a length of 128 pixels and a height of 64 pixels, which means it has that many
+separate LEDs that can be turned on and off in order to create a graphical image or text image. 
+That's 8,192 separate LEDs! 
+
+A communicating protocal that can be used between communicating devices help utilize gobs of LEDs.
+A protocol is a set of rules and guidelines for communicating data. 
+We want to design a protocol that can be used to communicate between the HERO and the OLED display. 
+Remember to install OLED library (<PololuOLED.h> and <U8glib-HAL.h>). Check Day16 to manually import .zip libraries. 
+
+I2C Communication Protocol sends data through a single wire.
+It carries a message in the form of data packets that are communicated between the two devices (HERO -> OLED) 
+using clock wire pulses and data wire pulses.
+
+OLED Display has 4 pins GND (0v /negative voltage supply), VCC (5v /positive voltage supply), SCL (Serial Clock), and SDA (Serial Data).
+
+GND will be connected to GND pin of the HERO Board, just like other components.
+VCC will be connected to 5v pin of the HERO Board, just like other components as well. 
+SCL will syncronize the devices. Connected to pin A5
+SDA will ***. Connected to pin A4.
+
+Pins A4 and A5 should be used for components requiring I2C communication such as the 128 x 64 OLED display. 
+*/
+
+// ********************************************************* //
+
+// #include <U8glib.h> causes Compilation error: U8glib.h: No such file or directory
+#include <U8glib-HAL.h>
+#include <PololuOLED.h>
+ 
+// Type of graphical object | name of our object of this type
+U8GLIB_SH1106_128X64  My_u8g_Panel(U8G_I2C_OPT_NONE); // I2C / TWI
+
+// Custom function that draws things on the OLED screen. Does not return values.
+// One of the things we can do is write one item of text on the screen in a particular location.  
+void draw(void) {
+  // graphic commands to redraw the complete screen should be placed here  
+  My_u8g_Panel.setFont(u8g_font_unifont); // unicode font 
+
+  // drawstring command that takes 3 different inputs.
+  // x position, Y position, and the actual string data that is to be printed out on the panel.
+  My_u8g_Panel.drawStr( 0, 10, "Build"); // upper left to right and down.
+  My_u8g_Panel.drawStr( 0, 30, "everything!");
+  My_u8g_Panel.drawStr( 0, 50, "Invent safe!!");
+}
+ 
+// ********************************************************* //
+
+void setup(void) {
+  // flip screen, if required
+  // My_u8g_Panel.setRot180(); // If we mount the panel upside down we might want to flip the display 180 degrees.
+ 
+  // assign default color value
+  My_u8g_Panel.setColorIndex(1); // We have a monochrome display that can display different grey levels. Default brightness is set to intermediate grey level 1. 
+ 
+ // pinMode(8, OUTPUT);
+}
+
+// ********************************************************* //
+
+void loop(void) {
+  // picture do-while loop will execute repeatedly until My_u8g_Panel.nextPage returns a 0, which means it's done.
+  My_u8g_Panel.firstPage();
+  do {
+    draw(); // call draw repeatedly until do loop finishes. 
+  } while( My_u8g_Panel.nextPage() );
+}
